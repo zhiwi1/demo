3f01553c8b9bc796474bf3ecbd053610069ead4b
zhiwi1 <56759239+zhiwi1@users.noreply.github.com>
2021-08-19 23:28:20 +0300

    test validators

--- src/test/java/com/epam/webproject/validator/AnswerValidatorTest.java
+++ src/test/java/com/epam/webproject/validator/AnswerValidatorTest.java
@@ -1,4 +1,28 @@
 package com.epam.webproject.validator;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class AnswerValidatorTest {
+    @DataProvider(name = "validAnswer")
+    public static Object[][] validAnswer() {
+        return new Object[][]{{"12"}, {"Work 1"}, {"math"}};
+    }
+
+    @Test(dataProvider = "validAnswer")
+    public void checkAnswerPositiveTest(String answer) {
+        Assert.assertTrue(AnswerValidator.checkAnswer(answer));
+    }
+
+    @DataProvider(name = "invalidAnswer")
+    public static Object[][] invalidAnswer() {
+        return new Object[][]{{""}, {"1"}, {null}};
+    }
+
+    @Test(dataProvider = "invalidAnswer")
+    public void checkAnswerNegativeTest(String answer) {
+        Assert.assertFalse(AnswerValidator.checkAnswer(answer));
+    }
+
 }
--- src/test/java/com/epam/webproject/validator/CommentValidatorTest.java
+++ src/test/java/com/epam/webproject/validator/CommentValidatorTest.java
@@ -1,4 +1,27 @@
 package com.epam.webproject.validator;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 public class CommentValidatorTest {
+    @DataProvider(name = "validComment")
+    public static Object[][] validComment() {
+        return new Object[][]{{"1"}, {"Work 1"}, {"math"}};
+    }
+
+    @Test(dataProvider = "validComment")
+    public void checkCommentPositiveTest(String comment) {
+        Assert.assertTrue(CommentValidator.checkComment(comment));
+    }
+    @DataProvider(name = "invalidComment")
+    public static Object[][] invalidComment() {
+        return new Object[][]{{""}, {null}};
+    }
+
+    @Test(dataProvider = "invalidComment")
+    public void checkCommentNegativeTest(String comment) {
+        Assert.assertFalse(CommentValidator.checkComment(comment));
+    }
+
 }
